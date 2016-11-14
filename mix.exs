cbf2d706c00bf67d8702a364fe9789b110d5d63f
Brad Condo <bradcondo@gmail.com>
2016-11-14 15:35:44 -0500

    Versioned to 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RedisEx.Mixfile do
 
   def project do
     [app: :redis_ex,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
